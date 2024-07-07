0fbb8d9e8be40ee876478617d2385ff1cb5afde8
andrew hwang <andrewwhwang@gmail.com>
2024-07-06 22:37:19 -0400

    finished part 2

--- 2023/02/code.go
+++ 2023/02/code.go
@@ -33,6 +33,29 @@ func valid_set(sets string) bool {
 	return true
 }
 
+func get_min_set(sets string) int {
+	min_set := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
+	}
+
+	for _, set := range strings.Split(sets, "; ") {
+		for _, draw := range strings.Split(set, ", ") {
+			draw_split := strings.Split(draw, " ")
+			num, _ := strconv.Atoi(draw_split[0])
+			color := draw_split[1]
+
+			value, _ := min_set[color]
+			if num > value {
+				min_set[color] = num
+			}
+		}
+	}
+
+	return min_set["red"] * min_set["green"] * min_set["blue"]
+}
+
 // on code change, run will be executed 4 times:
 // 1. with: false (part1), and example input
 // 2. with: true (part2), and example input
@@ -42,7 +65,14 @@ func valid_set(sets string) bool {
 func run(part2 bool, input string) any {
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
-		return "not implemented"
+		var part2_counter int
+
+		for _, line := range strings.Split(input, "\n") {
+			split := strings.Split(line, ": ")
+			sets := split[1]
+			part2_counter += get_min_set(sets)
+		}
+		return part2_counter
 	}
 	var counter int
 	for _, line := range strings.Split(input, "\n") {
